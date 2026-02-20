c99fc571319079cad169db34fe9a14b22dd19ecd
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-02-20 17:27:46 +0530

    Add comments explaining syntax errors and linter functionality in app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 # What is expression in python ;
 print("*" * 3)
+# What is a sytax error;
+# the syax error is which is produce by bad sytax and bad grammer in the code
+
+# What does a linter do ?
+# linter is tool which is use to figer out the pontial error in code mainly in sytax error;
