cf0953ee3b309a7c31724f746f9be9973fd4e80f
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-02-21 17:02:22 +0530

    Add additional arithmetic operations and variable manipulation examples in app.py

--- app.py
+++ app.py
@@ -8,3 +8,18 @@ print(course.rstrip())
 print(course.find("Pro"))
 print(course.replace("P","J"))
 print("Pro" in course)
+
+
+print(10 + 1)
+print(10 - 1)
+print(10 * 1)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x=10
+x = x+3
+x +=3
+print(x)
+ 
