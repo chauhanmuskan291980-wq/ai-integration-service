ba0762a072c8ea42821ee5e1eb4beea12c179e2f
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-02-21 13:28:59 +0530

    Remove commented explanations and retain core functionality in app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-# What is expression in python ;
-print("*" * 3)
-# What is a sytax error;
-# the syax error is which is produce by bad sytax and bad grammer in the code
-
-# What does a linter do ?
-# linter is tool which is use to figer out the pontial error in code mainly in sytax error;
-
-
+students_count = 1000
+rating = 4.99
+is_publish = False
+course_name = "Python Programming"
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(students_count)
+print(rating)
+print(is_publish)
+print(course_name)
