248ced082d92d0a6ebb38ac7b5b6975ebbbb0999
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-02-21 13:52:39 +0530

    Replace student-related variables and print statements with full name output in app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-students_count = 1000
-rating = 4.99
-is_publish = False
-course_name = "Python Programming"
-print(len(course_name))
-print(course_name[0])
-print(course_name[-1])
-print(course_name[0:-3])
-print(course_name[0:])
-print(course_name[:5])
+first = "Mosh"
+last = "Hamedani"
+full = f"{first}{last}"
+print(full)
