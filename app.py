cbc32567949e879fb22c9dd4fa57bb092a59d380
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-02-21 21:53:08 +0530

    Add eligibility evaluation logic for income and credit status in app.py

--- app.py
+++ app.py
@@ -55,3 +55,29 @@ elif temperature < 20 and temperature > 10:
 else:
     print("It's cold")
 print("Done")
+
+high_income = True
+good_credit = False
+
+if high_income and good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+
+high_income = True
+good_credit = False
+if high_income or good_credit:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+
+high_income = True
+good_credit = False
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not eligible")
